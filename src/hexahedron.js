82814072e63dbb75618cc5fa89e357c991cb196e
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-05-15 13:38:18 +0000

    changed node rectangle test app to hexahedron

--- src/hexahedron.js
+++ src/hexahedron.js
@@ -1,22 +1,21 @@
-const Hexahedron = require("../src").Hexahedron;
-const expect = require("chai").expect;
+class Hexahedron {
+  constructor(length) {
+    this.length = length;
+  }
 
-describe("Testing the Hexahedron Functions", function () {
-  it("1. The side length of the Hexahedron", function (done) {
-    let h1 = new Hexahedron(2);
-    expect(h1.getSideLength()).to.equal(2);
-    done();
-  });
+  getSideLength() {
+    return this.length;
+  }
 
-  it("2. The surface area of the Hexahedron", function (done) {
-    let h2 = new Hexahedron(5);
-    expect(h2.getSurfaceArea()).to.equal(150);
-    done();
-  });
+  getSurfaceArea() {
+    return this.length * this.length * 6;
+  }
 
-  it("3. The volume of the Cube", function (done) {
-    let h3 = new Hexahedron(7);
-    expect(h3.getVolume()).to.equal(343);
-    done();
-  });
-});
+  getVolume() {
+    return Math.pow(this.length, 3);
+  }
+}
+
+module.exports = {
+  Hexahedron: Hexahedron,
+};
