c045ad11e2ff526c42cf7ac4c80a8cf4ad14e7b4
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-05-19 12:19:21 +0000

    handling async functions

---
+++ test/asyncawait.test.js
@@ -0,0 +1,13 @@
+// test/asyncawait.test.js
+import("chai").then((chai) => {
+  const expect = chai.expect;
+
+  const fetchData = require("../asyncawait");
+
+  describe("Asynchronous Test with Async/Await", function () {
+    it("should fetch data correctly", async function () {
+      const data = await fetchData();
+      expect(data).to.equal("data");
+    });
+  });
+});
